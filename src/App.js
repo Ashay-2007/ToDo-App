ca5ab283f35357617409af373c95189c87e76c1b
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-01-30 09:42:26 +0530

    added List to display items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <h1>Hello World</h1>
       <input type="text"/>
       <button>Add Todo</button>
 
-      
+      <ul>
+        <li>Take dogs for a walk</li>
+        <li>Take the rubbish out</li>
+      </ul>
     </div>
   );
 }
