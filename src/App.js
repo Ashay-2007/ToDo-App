31e4f65b8443871f93028d4359c2a12175d5acc6
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-01-30 20:00:22 +0530

    added button using materialUI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { useState} from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out', 'I want to eat out today']);
   const [input, setInput] = useState('');
 
   const addTodo = (event) => {
-    //this will fire up when we click the button
+    //this function will fire up when we click the button
+    event.preventDefault(); //This prevents page from refreshing every time todo is added using addTodo button
     setTodos([...todos, input]);
+    setInput(''); //clear up the input field after each add
   }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button onClick={addTodo}>Add Todo</button>
-
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <Button type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
+        
+      </form>
+      
       <ul>
         {todos.map(todo => (
           <li>{todo}</li>
