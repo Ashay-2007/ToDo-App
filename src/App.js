91225a90c47c040e24b61cb096950644fbeff67c
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-01-30 11:13:06 +0530

    adding variables to connect addTodobutton

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { useState} from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out', 'I want to eat out today']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    //this will fire up when we click the button
+  }
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <input type="text"/>
-      <button>Add Todo</button>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
 
       <ul>
-        <li>Take dogs for a walk</li>
-        <li>Take the rubbish out</li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
       </ul>
     </div>
   );
